86d0bae725e3872f31f15344cd105534fdf6feca
Jake Stetter <jake_stetter@hotmail.com>
2022-06-09 13:04:31 -0700

    update

---
+++ CoreApiFundamentals-3.1/src/Controllers/CampsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCodeCamp.Controllers
+{
+	[Route("api/[controller]")]
+
+
+	public class CampsController : ControllerBase
+	{
+		public object Get()
+        {
+			return new { Moniker = "ATL2018", Name = "Atlanta Code Camp" };
+        }
+	}
+}
+
